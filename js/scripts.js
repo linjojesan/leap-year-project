78f33c0297e978b7af2a318556588c4ef69c7acb
John Linneman <urielpro78@gmail.com>
2018-03-27 22:20:05 -0700

    add span tags to create a is not or is a leap year message

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,16 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
 
   });
 });
@@ -13,9 +22,7 @@ $(document).ready(function() {
 
 
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-  }  return false;
-  (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
